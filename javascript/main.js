4c6e7d38f9efe0c2f41b6f235351321369cb00a1
vnphat <vnpaht75@gmail.com>
2017-07-01 09:35:13 +0700

    complate

--- javascript/main.js
+++ javascript/main.js
@@ -74,7 +74,7 @@
         });
 
         $(document).on('click', '#mainnav-mobi li .btn-submenu', function(e) {
-            $(this).toggleClass('active').next('.sub-menu').slideToggle(300);
+            $(this).toggleClass('active').next('ul').slideToggle(300);
             e.stopImmediatePropagation()
         });
     }
@@ -112,138 +112,6 @@
             return false;
         });
     };
-
-    var flatClientCarousel = function() {
-        $('.flat-carousel').each(function(){
-            if ( $().owlCarousel ) {
-                $(this).find('.owl-carousel-client').owlCarousel({
-                    loop: true,
-                    margin:10,
-                    auto: true,
-                    dots: false,
-                    responsive:{
-                        0:{
-                            items: 1
-                        },
-                        480:{
-                            items: 2
-                        },
-                        767:{
-                            items: 3
-                        },
-                        991:{
-                            items: 3
-                        }, 
-                        1200:{
-                            items: 5
-                        }               
-                    }
-                });
-            }
-        });
-    };
-
-    var flatClientCompany = function() {
-        $('.flat-carousel').each(function(){
-            if ( $().owlCarousel ) {
-                $(this).find('.owl-carousel-company').owlCarousel({
-                    loop: true,
-                    margin:10,
-                    auto: true,
-                    nav:true,
-                    dots: false,
-                    responsive:{
-                        0:{
-                            items: 1
-                        },
-                        480:{
-                            items: 2
-                        },
-                        767:{
-                            items: 3
-                        },
-                        991:{
-                            items: 3
-                        }, 
-                        1200:{
-                            items: 6
-                        }               
-                    }
-                });
-            }
-        });
-    };
-
-    var flatTabs = function () {
-        $('.flat-tabs').each(function() {
-            $(this).children('.content-tab').children().hide();
-            $(this).children('.content-tab').children().first().show();
-
-            $(this).find('.menu-tabs').children('li').on('click', function(e) {
-                var liActive = $(this).index(),
-                    contentActive = $(this).siblings().removeClass('active').parents('.flat-tabs').children('.content-tab').children().eq(liActive);
-
-                contentActive.addClass('active').fadeIn('slow');
-                contentActive.siblings().removeClass('active');
-                $(this).addClass('active').parents('.flat-tabs').children('.content-tab').children().eq(liActive).siblings().hide();
-                e.preventDefault();
-            });
-        });
-    };
-
-    var responsiveSlider = function() {
-        var menuType = 'desktop';
-
-        $(window).on('load resize', function() {
-            var currMenuType = 'desktop';
-            if ( matchMedia( 'only screen and (max-width:1199px)' ).matches ) {
-                currMenuType = 'tablet';
-            }
-
-            if ( matchMedia( 'only screen and (max-width:767px)' ).matches ) {
-                currMenuType = 'mobile';
-            }
-
-            if ( matchMedia( 'only screen and (max-width:320px)' ).matches ) {
-                currMenuType = 'iphone';
-            }
-
-
-
-            if ( currMenuType !== menuType ) {
-                menuType = currMenuType;
-
-                if ( currMenuType === 'tablet' ) {
-                     $(".title-slider").attr('data-x','100');
-                     $(".sub-title-slider").attr('data-x','100');
-                     $(".button-slider").attr('data-x','100');
-                } 
-                else {
-                    $(".title-slider").attr('data-x','15');
-                     $(".sub-title-slider").attr('data-x','15');
-                     $(".button-slider").attr('data-x','15');
-
-                    if ( currMenuType === 'mobile' ) {
-                        $(".title-slider").attr('data-y','155');
-                        $(".sub-title-slider").attr('data-y','233');
-                        $(".button-slider").attr('data-y','370');
-                    }
-                    else
-                        if ( currMenuType === 'iphone' ) {
-                            $(".title-slider").attr('data-y','80');
-                            $(".sub-title-slider").attr('data-y','150');
-                            $(".button-slider").attr('data-y','390');
-                        }
-                        else {
-                            $(".title-slider").attr('data-y','255');
-                            $(".sub-title-slider").attr('data-y','333');
-                            $(".button-slider").attr('data-y','450');
-                        }
-                 }
-            }
-        });
-
-    }
 
     var flatSearch = function () {
         $(document).on('click', function(e) {   
@@ -356,13 +224,11 @@
         dreamSliderpages();
         inspiredDreamslider();
         menuSidebar();
-        responsiveSlider();
         responsiveMenu();
-        flatClientCarousel();
-        flatClientCompany();
         flatSearch();
-        flatTabs();
-        //removePreloader();
+        $('.button-close').click(function() {
+            $(this).parent().parent().addClass('hidden');
+        });
    	});
 
 })(jQuery);
